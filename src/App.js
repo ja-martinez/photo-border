32d9018baa9401a6f6e5f2bb8b240dcea0c2b519
clavius <16825879+ja-martinez@users.noreply.github.com>
2023-03-29 17:01:51 -0700

    abstract logic for image input into custom hook

--- src/App.js
+++ src/App.js
@@ -4,58 +4,17 @@ put state in reducer
 add url as id to reset editor
 */
 
-import { useState, useEffect } from "react";
+import useImageInput from "./Hooks/useImageInput";
 import "./App.css";
 import Editor from "./Components/Editor";
 import InitialPage from "./Components/InitialPage";
 
 function App() {
-  const [imageFile, setImageFile] = useState();
-  const [image, setImage] = useState();
-
-  // have to use effect since image loading is async and may set state when component unmounts or imageFile changes
-  useEffect(() => {
-    if (!imageFile) return  // there's no file
-
-    const newImage = new Image();
-    newImage.src = URL.createObjectURL(imageFile);
-    console.log(newImage.src)
-
-    newImage.onload = () => {
-      URL.revokeObjectURL(newImage.src); // don't need URL anymore
-      setImage(newImage);
-    };
-
-    return () => {
-      // if a new image was being loaded, stop it.
-
-      if (newImage.complete) return; // finished loading, nothing to do
-
-      newImage.onload = null;
-      const newImageSource = newImage.src;
-      newImage.src = ""; // triggers load, so do it after clearing onload
-      URL.revokeObjectURL(newImageSource); // after unsetting src to prevent loading errors
-    };
-  }, [imageFile]);
-
-  function onFileChange(e) {
-    const currFile = e.target.files[0];
-
-    if (!currFile) {
-      return;
-    }
-
-    if (!isValidImage(currFile)) {
-      // TODO:
-      throw new Error("invalid image type");
-    }
-
-    setImageFile(currFile);
-  }
+  const {image, onFileChange, isLoading} = useImageInput();
   
   let content;
   if (!image) {
-    content = <InitialPage onFileChange={onFileChange} isLoading={imageFile === true} />;
+    content = <InitialPage onFileChange={onFileChange} isLoading={isLoading} />;
   } else {
     content = <Editor image={image} onFileChange={onFileChange} key={image.src} />;
   }
@@ -64,13 +23,3 @@ function App() {
 }
 
 export default App;
-
-// Utilities
-
-function isValidImage(file) {
-  // may have to have specific file types
-  if (file.type.startsWith("image/")) {
-    return true;
-  }
-  return false;
-}
---
+++ src/Hooks/useImageInput.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+
+function useImageInput() {
+  const [imageFile, setImageFile] = useState();
+  const [image, setImage] = useState();
+
+  let isLoading = !image && imageFile;
+
+  // have to use effect since image loading is async and may set state when component unmounts or imageFile changes
+  useEffect(() => {
+    if (!imageFile) return  // there's no file
+
+    const newImage = new Image();
+    newImage.src = URL.createObjectURL(imageFile);
+
+    newImage.onload = () => {
+      URL.revokeObjectURL(newImage.src); // don't need URL anymore
+      setImage(newImage);
+    };
+
+    return () => {
+      // if a new image was being loaded, stop it.
+
+      if (newImage.complete) return; // finished loading, nothing to do
+
+      newImage.onload = null;
+      const newImageSource = newImage.src;
+      newImage.src = ""; // triggers load, so do it after clearing onload
+      URL.revokeObjectURL(newImageSource); // after unsetting src to prevent loading errors
+    };
+  }, [imageFile]);
+
+  function onFileChange(e) {
+    const currFile = e.target.files[0];
+
+    if (!currFile) {
+      return;
+    }
+
+    if (!isValidImage(currFile)) {
+      // TODO:
+      throw new Error("invalid image type");
+    }
+
+    setImageFile(currFile);
+  }
+
+  return {
+    image,
+    onFileChange,
+    isLoading
+  }
+}
+
+export default useImageInput;
+
+// Utilities
+
+function isValidImage(file) {
+  // may need to specify file types in future
+  if (file.type.startsWith("image/")) {
+    return true;
+  }
+  return false;
+}
