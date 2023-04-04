510d4ec4ec8f9a2c047e5674389f21bd2944144c
clavius <16825879+ja-martinez@users.noreply.github.com>
2023-04-03 20:28:15 -0700

    memoize ColorSection component

--- src/Components/ColorSection.js
+++ src/Components/ColorSection.js
@@ -1,12 +1,14 @@
+import { memo } from "react";
+
 import WorkAreaSection from "./WorkAreaSection";
 import ColorPicker from "./ColorPicker";
 
-function ColorSection({ color, setColor }) {
+const ColorSection = memo(function ColorSection({ color, setColor }) {
   return (
     <WorkAreaSection sectionTitle={"Color"}>
       <ColorPicker color={color} setColor={setColor} />
     </WorkAreaSection>
   );
-}
+});
 
 export default ColorSection;
